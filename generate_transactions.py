148df03755a870b2bfb6471d94381c927d75d87b
Alekya2305 <alekyamothuri@oakland.edu>
2026-06-16 12:55:35 -0400

    Level:1 generate 1000 fake bank transactions

---
+++ generate_transactions.py
@@ -0,0 +1,27 @@
+import random
+from faker import Faker
+import csv
+
+fake = Faker()
+
+transactions = []
+
+for i in range(1000):
+    transaction = {
+        "transaction_id": i + 1,
+        "user_id": random.randint(1,100) ,
+        "amount": round(random.uniform(1,1000),2),
+        "category" : random.choice(["Food", "Entertainment", "Shopping", "Bills"]),
+        "merchant": fake.company(),
+        "timestamp": fake.date_time_this_year().strftime("%Y-%M-%D %H-%M-%S"),
+        "isFrauad": random.choice(["True", "False"]),
+        }
+    transactions.append(transaction)
+    
+with open("transactions.csv", "w", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=transactions[0].keys())
+    writer.writeheader()
+    writer.writerows(transactions)
+
+print(f"Done, Generated {len(transactions)} transactions")
+print("First transaction:", transactions[0])
--- hello_lime.py
+++ hello_lime.py
@@ -1,1 +1,9 @@
-print("Hello, Lime Data Engineer")
+transaction_id = 1
+amount = 49.9
+merchant = "Starbucks"
+is_Fraud = False
+
+print("Transaction: ", transaction_id)
+print("Amount: ", amount)
+print("Merchant: ", merchant)
+print("Fraud? ", is_Fraud)
